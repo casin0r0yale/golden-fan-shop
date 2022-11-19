12db4168a0bd8e304c4c786f9fd0c2a644502a25
Daniel Halper <daniel@stepuptutoring.org>
2022-11-19 12:07:58 -0800

    Added .env file

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 // EXPRESS SERVER Index
 
 const express = require ("express");
-
+const axios = require ("axios")
 const app = express();
 
 app.use(express.json());
@@ -10,3 +10,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static('/Users/admin/Desktop/RPP2207 Sprints/golden-fan-shop/client/dist'));
 
 app.listen(3000, () => console.log('Our Server is listening on port 3000...'));
+
+app.get('/getProducts', (req, res) => {
+  
+  app.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products')
+  res.send('hello world')
+})
