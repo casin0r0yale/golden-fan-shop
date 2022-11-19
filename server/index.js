248669ea21a0b6b4834c01be68ee066fa9caf7ac
Richard Tao Wang <richardtaowang@gmail.com>
2022-11-19 23:28:42 +0300

    Flexible dirname and axios added

--- server/index.js
+++ server/index.js
@@ -7,12 +7,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static('/Users/admin/Desktop/RPP2207 Sprints/golden-fan-shop/client/dist'));
+app.use(express.static(__dirname + '/../client/dist'));
 
 app.listen(3000, () => console.log('Our Server is listening on port 3000...'));
 
 app.get('/getProducts', (req, res) => {
-  
+
   app.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products')
   res.send('hello world')
 })
