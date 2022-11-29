32fd5ced315ec129d90afc290af14c6f02168017
Daniel Halper <daniel@stepuptutoring.org>
2022-11-29 15:32:04 -0800

    Created grid for overview component

--- client/src/components/overview.jsx
+++ client/src/components/overview.jsx
@@ -1,11 +1,19 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import "../styles/index.css";
 
 
 const Overview = (props) => {
 
   return (
-    <h1>CSS Test</h1>
+    <div>
+      <h1>CSS Test</h1>
+      <div class="overviewContainer">
+      <div class="productInfo">product info </div>
+      <div class="styleSelector">styleSelector </div>
+      <div class="addToCart">addToCart </div>
+      <div class="imageGallery">imageGallery </div>
+      </div>
+    </div>
   )
 }
 
