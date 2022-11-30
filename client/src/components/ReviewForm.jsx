28850131101a5ee3ccb8719d5aa9168c8bd540bc
Tony Vo <vo.tony12@gmail.com>
2022-11-30 15:58:23 -0700

    implemented 'Add a review' button that loads Review Form Component

---
+++ client/src/components/ReviewForm.jsx
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from 'react';
+
+const ReviewForm = (props) => {
+  return (
+    <div>Form!</div>
+  )
+}
+
+export default ReviewForm
--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -1,9 +1,20 @@
 import React, { useState, useEffect } from 'react';
+import ReviewForm from './ReviewForm.jsx';
 
 const Reviews = (props) => {
 
+  const [formView, setFormView] = useState(false)
+
   return (
-    <div>Reviews</div>
+    <div>
+      <div id="review-list">
+        <h3>Review List Module goes here</h3>
+      </div>
+      {(formView === false) ? null :  <ReviewForm/>}
+      <button onClick={() => setFormView(!formView)}>
+        Add a review
+      </button>
+    </div>
   )
 }
 
