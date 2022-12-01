91b0f39a3894e57461ce3c5e149cc5ac9c04d461
Tony Vo <vo.tony12@gmail.com>
2022-11-30 17:59:51 -0700

    implemented Summary and Body form inputs

--- client/src/components/ReviewForm.jsx
+++ client/src/components/ReviewForm.jsx
@@ -1,6 +1,15 @@
 import React, {useState, useEffect} from 'react';
 
 const ReviewForm = (props) => {
+
+  const [charsLeft, setMinCharsLeft] = useState(50);
+
+  const handleCharCount = (event) => {
+    const charCount = event.target.value.length;
+    const curMinCharsLeft = 50 - charCount;
+    setMinCharsLeft(curMinCharsLeft);
+  }
+
   return (
     <div>
       <h2>Write Your Review</h2>
@@ -13,8 +22,28 @@ const ReviewForm = (props) => {
           <input type="radio" id="no" name="recommend" value="no" required/>
           <label>no</label>
         </label>
-        <br></br>
-        <br></br>
+          <br></br>
+          <br></br>
+        <label>
+          Review Summary
+            <br></br>
+          <textarea type="text" name="summary" maxLength="60">
+            Example: Best Purchase Ever!
+          </textarea>
+        </label>
+          <br></br>
+          <br></br>
+        <label>
+          Review Body
+          <br></br>
+          <textarea type="text" name="body" minLength="50" maxLength="1000" onChange={handleCharCount}>
+            Why did you like the product or not?
+          </textarea>
+          {(charsLeft > 0) ? <p> Minimum required characters left: {charsLeft}</p> : null}
+          {(charsLeft <= 0) ? <p>Minimum reached</p> : null}
+        </label>
+          <br></br>
+          <br></br>
         <label>
           What is your nickname?*
           <input type="text" name="nickname" placeholder="Example: jackson11!" maxLength="60" required/>
