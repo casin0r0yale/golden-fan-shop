582fb1328db39968bf1c3619f4f0ec48b025f320
Stefan Trueitt <stefantrueittbusiness@gmail.com>
2022-12-01 22:54:51 -0500

    Updating local project to the main version

--- client/src/components/RelatedCard.jsx
+++ client/src/components/RelatedCard.jsx
@@ -2,14 +2,18 @@ import React, {useState, useEffect} from 'react';
 
 const RelatedCard = (props) => {
 
+  // {props.related_id} also can passed in
+
   return (
-    <div className='box'>
-      <div>RelatedCard: Picture on Top</div>
-      <div>Star Action Button</div>
-      <div>"category"</div>
-      <div>Product name with extra text</div>
-      <div>Price</div>
-      <div>Stars/Reviews</div>
+    <div className='productCard'>
+      <div>
+        <img src={props.related_thumbnail}/>
+      </div>
+      <div>TODO: Action Button</div>
+      <div>{props.related_category}</div>
+      <div>{props.related_name}</div>
+      <div>{props.related_price}</div>
+      <div>TODO: Stars/Reviews</div>
     </div>
   )
 }
