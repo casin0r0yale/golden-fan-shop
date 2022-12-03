f3bb52a0cc302bea1fec33b3e0d65c3085c8c637
Tony Vo <vo.tony12@gmail.com>
2022-12-03 14:24:27 -0700

    started image upload component

--- client/src/components/reviews/ReviewForm.jsx
+++ client/src/components/reviews/ReviewForm.jsx
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import StarRating from './StarRating.jsx';
+import SingleImageUpload from './SingleImageUpload.jsx';
 
 const ReviewForm = (props) => {
 
@@ -102,6 +103,7 @@ const ReviewForm = (props) => {
         <label>
           Overall Rating*
           <StarRating currRating={currRating}/>
+          {/**have an invisible input here that takes in rating and SHOULD be required */}
         </label>
         <br></br>
         <label>
@@ -231,10 +233,16 @@ const ReviewForm = (props) => {
           {(charsLeft > 0) ? <p> Minimum required characters left: {charsLeft}</p> : null}
           {(charsLeft <= 0) ? <p>Minimum reached</p> : null}
         </label>
+        <label>
+          Upload Your Photos
+          <SingleImageUpload />
+          {/* <input type="file" id="photos" name="photos" accept="image/png, image/jpeg" multiple/> */}
+        </label>
           <br></br>
           <br></br>
         <label>
           What is your nickname?*
+          <br></br>
           <input type="text" name="nickname" placeholder="Example: jackson11!" maxLength="60" required/>
           <h5>For privacy reasons, do not use your full name or email address</h5>
         </label>
