8a471f9b3b357729107d8ffc14dff3bd76d81906
Tony Vo <vo.tony12@gmail.com>
2022-12-06 15:15:57 -0700

    implemented star rating icons for products

--- client/src/components/reviews/ProductRating.jsx
+++ client/src/components/reviews/ProductRating.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import { IoStarSharp, IoStarOutline, IoStarHalfSharp } from 'react-icons/io5';
 
 const ProductRating = (props) => {
-  console.log('this is the rating: ', props.rating)
+  var rating = props.rating;
+  var ratingRoundDown = Math.floor(rating);
+
+  const createStars = (number) => {
+    var array = [];
+    for (var i = 0; i < number; i++) {
+      array.push(<IoStarSharp />);
+    };
+    if (rating % 1 !== 0) {
+      array.push(<IoStarHalfSharp />)
+      let remainingStarCount = Math.floor(5 - rating);
+      for (var j = 0; j < remainingStarCount; j++) {
+        array.push(<IoStarOutline />);
+      }
+    } else {
+      let remainingStarCount = 5 - rating;
+      for (var k = 0; k < remainingStarCount; k++) {
+        array.push(<IoStarOutline />);
+      }
+    }
+    return array;
+  }
+  var stars = createStars(ratingRoundDown);
+
   return (
     <div>
-      <h1>{props.rating}</h1>
+      <h1>{rating}</h1>
+      <span>{stars}</span>
     </div>
   )
 }
--- client/src/components/reviews/StarRating.jsx
+++ client/src/components/reviews/StarRating.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BsStar } from 'react-icons/bs';
+// import { BsStar } from 'react-icons/bs';
 
 const StarRating = (props) => {
 
