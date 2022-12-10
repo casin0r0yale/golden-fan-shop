6fda12667dc83d96210c2502626c7c8fbf685454
Tony Vo <vo.tony12@gmail.com>
2022-12-10 13:51:25 -0700

    fixed star rating breakdown order

--- client/src/components/reviews/RatingBreakdown.jsx
+++ client/src/components/reviews/RatingBreakdown.jsx
@@ -6,7 +6,8 @@ import PercentageBar from './PercentageBar.jsx';
 const RatingBreakdown = (props) => {
   var reviewList = props.reviewList;
   var numReviews = reviewList.length;
-  // console.log('break down props: ', reviewList);
+  var numRecommend = 0;
+  console.log('break down props: ', reviewList);
 
   const ratingPercentage = {
     1: [0],
@@ -22,10 +23,12 @@ const RatingBreakdown = (props) => {
 
   const getAllPercentage = (array) => {
     array.forEach((review) => {
+      if (review.recommend) {
+        numRecommend++
+      }
       ratingPercentage[review.rating][0]++
     });
     for (var rating in ratingPercentage) {
-      // ratingPercentage[rating] = getPercentage(ratingPercentage[rating]);
       var percentage = getPercentage(ratingPercentage[rating]);
       ratingPercentage[rating].push(percentage);
     }
@@ -33,10 +36,6 @@ const RatingBreakdown = (props) => {
   }
 
   var currPercentageArr = Object.values(getAllPercentage(reviewList));
-
-  console.log('this is the current percentage: ', currPercentageArr);
-
-
   return (
     <div>
       <p>RATINGS & REVIEWS</p>
@@ -45,9 +44,11 @@ const RatingBreakdown = (props) => {
         <ProductRating rating={props.rating}/>
       </div>
       <div>
-        <p>100% of reviews recommend this product</p>
-        {currPercentageArr.map((rating, idx) => {
-          return <p>{idx + 1} stars <PercentageBar bgcolor={'#59981A'} completed={rating[1]}/>{rating[0]} reviews</p>
+        <h3>Rating Breakdown</h3>
+        <p>{(numRecommend / numReviews) * 100}% of reviews recommend this product</p>
+        {currPercentageArr.slice(0).reverse().map((rating, idx) => {
+          return <p onClick={() => console.log('clicked on this star:', currPercentageArr.length +  1 - 1 - idx)}>{currPercentageArr.length +  1 - 1 - idx} stars
+          <PercentageBar bgcolor={'#59981A'} completed={rating[1]}/>{rating[0]} reviews</p>
         })}
       </div>
       <CharacteristicsBreakdown reviewList={props.reviewList}/>
