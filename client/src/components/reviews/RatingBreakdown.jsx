7ea2e17d2d0b1dfb8ed57b06b1bb087075564ebe
Tony Vo <vo.tony12@gmail.com>
2022-12-10 13:24:57 -0700

    refactored ratings bar and added number of reviews to bar

--- client/src/components/reviews/RatingBreakdown.jsx
+++ client/src/components/reviews/RatingBreakdown.jsx
@@ -9,11 +9,11 @@ const RatingBreakdown = (props) => {
   // console.log('break down props: ', reviewList);
 
   const ratingPercentage = {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0
+    1: [0],
+    2: [0],
+    3: [0],
+    4: [0],
+    5: [0]
   };
 
   const getPercentage = (tally) => {
@@ -22,10 +22,12 @@ const RatingBreakdown = (props) => {
 
   const getAllPercentage = (array) => {
     array.forEach((review) => {
-      ratingPercentage[review.rating]++
+      ratingPercentage[review.rating][0]++
     });
     for (var rating in ratingPercentage) {
-      ratingPercentage[rating] = getPercentage(ratingPercentage[rating]);
+      // ratingPercentage[rating] = getPercentage(ratingPercentage[rating]);
+      var percentage = getPercentage(ratingPercentage[rating]);
+      ratingPercentage[rating].push(percentage);
     }
     return ratingPercentage;
   }
@@ -33,12 +35,6 @@ const RatingBreakdown = (props) => {
   var currPercentageArr = Object.values(getAllPercentage(reviewList));
 
   console.log('this is the current percentage: ', currPercentageArr);
-
-  const testData = [
-    { bgcolor: "#6a1b9a", completed: 60 },
-    { bgcolor: "#00695c", completed: 30 },
-    { bgcolor: "#ef6c00", completed: 53 },
-  ];
 
 
   return (
@@ -50,11 +46,9 @@ const RatingBreakdown = (props) => {
       </div>
       <div>
         <p>100% of reviews recommend this product</p>
-          <p>5 stars<PercentageBar bgcolor={'#59981A'} completed={currPercentageArr[0]}/></p>
-          <p>4 stars<PercentageBar bgcolor={'#59981A'} completed={currPercentageArr[1]}/></p>
-          <p>3 stars<PercentageBar bgcolor={'#59981A'} completed={currPercentageArr[2]}/></p>
-          <p>2 stars<PercentageBar bgcolor={'#59981A'} completed={currPercentageArr[3]}/></p>
-          <p>1 stars<PercentageBar bgcolor={'#59981A'} completed={currPercentageArr[4]}/></p>
+        {currPercentageArr.map((rating, idx) => {
+          return <p>{idx + 1} stars <PercentageBar bgcolor={'#59981A'} completed={rating[1]}/>{rating[0]} reviews</p>
+        })}
       </div>
       <CharacteristicsBreakdown reviewList={props.reviewList}/>
     </div>
