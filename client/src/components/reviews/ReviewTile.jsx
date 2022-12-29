61f23dfc8fa3b113c26dc1074883ee4e4a4827a2
Tony Vo <vo.tony12@gmail.com>
2022-12-28 17:08:39 -0700

    Fixed merge conflicts

--- client/src/components/reviews/ReviewTile.jsx
+++ client/src/components/reviews/ReviewTile.jsx
@@ -39,22 +39,81 @@ const ReviewTile = (props) => {
   }
 
   return(
+    <div data-testid="review-tile">
+      <div className="review-name-date">
+        <ProductRating rating={props.review.rating} />
+        <p><AiFillCheckCircle />&nbsp;{props.review.reviewer_name}, {date}</p>
+      </div>
+      <h5>{props.review.summary}</h5>
+      <div data-testid="review-images">
+        {(photoArr.length !== 0) ? photoArr.map((url, idx) => {
+          return <img className="review-image"src={url} key={idx}/>
+        }) : null}
+      </div>
+      <p>{props.review.body}</p>
+      <div className="helpful-report">
+        <p>Helpful? <a onClick={() => helpClick(props.review.review_id)}>Yes({(helpful) ? props.review.helpfulness + 1 : props.review.helpfulness})</a>&nbsp;|&nbsp;</p>
+        <p onClick={() => reportClick(props.review.review_id)}>Report</p>
+      </div>
+
+    </div>
+  )
+}
+import React, { useState, useEffect, useRef } from 'react';
+import ProductRating from './ProductRating.jsx';
+import { format } from 'date-fns';
+import { AiFillCheckCircle } from 'react-icons/ai'
+
+const ReviewTile = (props) => {
+  const date = format(new Date(props.review.date), 'LLLL d, yyyy');
+  const photoArr = [];
+  if (props.review.photos.length !== 0) {
+    const photos = props.review.photos;
+    photos.forEach((photo) => {
+      photoArr.push(photo.url);
+    });
+  }
+
+  const [helpful, setHelpful] = useState(false);
+  var helpfulClickCount = 0;
+  var helpRef = useRef(0);
+
+  useEffect(() => {
+
+  }, [helpfulClickCount])
+
+  const helpClick = async (review_id) => {
+    // console.log('review id: ', review_id);
+    helpfulClickCount++
+    helpRef.current++
+    if (helpRef.current === 1) {
+      setHelpful(true);
+      await props.handleHelpClick(review_id);
+    } else if (helpRef.current > 1){
+      alert('Cannot vote more than once!');
+    }
+  }
+
+  const reportClick = async (review_id) => {
+    await props.handleReportClick(review_id);
+    alert('Review has been reported');
+  }
+
+  return(
     <div widgetname="Review Tile" data-testid="review-tile">
       <div widgetname="Review Tile" className="review-name-date">
         <ProductRating widgetname="Review Tile" rating={props.review.rating} />
         <p widgetname="Review Tile"><AiFillCheckCircle />&nbsp;{props.review.reviewer_name}, {date}</p>
       </div>
-<<<<<<< HEAD
       <h5 widgetname="Review Tile">{props.review.summary}</h5>
       <div widgetname="Review Tile" data-testid="review-images">
         {(photoArr.length !== 0) ? photoArr.map((url, idx) => {
           return <img widgetname="Review Tile" className="review-image"src={url} key={idx}/>
-=======
-      <h5>{props.review.summary}</h5>
-      <div data-testid="review-images">
-        {(photoArr.length !== 0) ? photoArr.map((url, idx) => {
-          return <img className="review-image"src={url} key={idx}/>
->>>>>>> main
+          <h5>{props.review.summary}</h5>
+          <div data-testid="review-images">
+            {(photoArr.length !== 0) ? photoArr.map((url, idx) => {
+              return <img className="review-image"src={url} key={idx}/>
+    >>>>>>> main
         }) : null}
       </div>
       <p widgetname="Review Tile">{props.review.body}</p>
