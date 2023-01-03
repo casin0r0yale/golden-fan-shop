e7acd614f9562341e5248dfe5d35a2968eb36c37
Tony Vo <vo.tony12@gmail.com>
2023-01-03 16:35:09 -0700

    added review image pop up

--- client/src/components/reviews/ReviewTile.jsx
+++ client/src/components/reviews/ReviewTile.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect, useRef } from 'react';
 import ProductRating from './ProductRating.jsx';
+import Popup from '../Popup.jsx';
 import { format } from 'date-fns';
 import { AiFillCheckCircle } from 'react-icons/ai'
 
 const ReviewTile = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [imgLink, setImgLink] = useState('');
   const date = format(new Date(props.review.date), 'LLLL d, yyyy');
   const photoArr = [];
   if (props.review.photos.length !== 0) {
@@ -38,6 +41,15 @@ const ReviewTile = (props) => {
     alert('Review has been reported');
   }
 
+  const handleImgPopUp = (link) => {
+    setIsOpen(true);
+    setImgLink(link);
+  }
+
+  const togglePopup = () => {
+    setIsOpen(!isOpen);
+  }
+
   return(
     <div widgetname="Review Tile" data-testid="review-tile">
       <div widgetname="Review Tile" className="review-name-date">
@@ -47,7 +59,7 @@ const ReviewTile = (props) => {
       <h5 widgetname="Review Tile">{props.review.summary}</h5>
       <div widgetname="Review Tile" data-testid="review-images">
         {(photoArr.length !== 0) ? photoArr.map((url, idx) => {
-          return <img widgetname="Review Tile" className="review-image"src={url} key={idx}/>
+          return <img widgetname="Review Tile" className="review-image" src={url} key={idx} onClick={() => handleImgPopUp(url)}/>
         }) : null}
       </div>
       <p widgetname="Review Tile">{props.review.body}</p>
@@ -55,6 +67,12 @@ const ReviewTile = (props) => {
         <p widgetname="Review Tile">Helpful? <a widgetname="Review Tile" onClick={() => helpClick(props.review.review_id)}>Yes({(helpful) ? props.review.helpfulness + 1 : props.review.helpfulness})</a>&nbsp;|&nbsp;</p>
         <p widgetname="Review Tile" onClick={() => reportClick(props.review.review_id)}>Report</p>
       </div>
+      {isOpen && <Popup
+        content={<>
+          <img src={imgLink} />
+        </>}
+        handleClose={togglePopup}
+      />}
 
     </div>
   )
